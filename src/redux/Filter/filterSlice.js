a4157ad2dd0002dc5245ff17f6bd38c670629517
vilhota30 <111867115+vilhota30@users.noreply.github.com>
2023-08-20 17:00:42 +0300

    налаштовує логіку форми

---
+++ src/redux/Filter/filterSlice.js
@@ -0,0 +1,15 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const filterSlice = createSlice({
+    name: "filter",
+    initialState: "",
+    reducers: {
+        setFilterValue(state, action) {
+            return state = action.payload;
+        },
+    },
+})
+
+export const filterReducer = filterSlice.reducer;
+
+export const {setFilterValue} = filterSlice.actions;
---
+++ src/redux/store.js
@@ -0,0 +1,37 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { contactsReducer } from "./Contacts/contactsSlice";
+import { filterReducer } from "./Filter/filterSlice";
+import { persistStore, persistReducer, 
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,} from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['contacts'],
+};
+
+const rootReducer = combineReducers({
+    contacts: contactsReducer,
+    filter: filterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+
+});
+
+export const persistor = persistStore(store);
